e539feec500d0286be9d80e855a33ac705060841
johnfitz3 <120284650+johnfitz3@users.noreply.github.com>
2023-01-26 17:46:11 -0500

    updating repo

---
+++ script.js
@@ -0,0 +1,8 @@
+function questionOne() {
+    
+    var x = document.createElement('DIV');
+    var t = document.createTextNode("Question 1");
+    x.setAttribute("style", "color: white;");
+    x.appendChild(t);
+    document.body.appendChild(x);
+}
