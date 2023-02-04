d5ce0e705e62ae32a88253426920b8299e379049
johnfitz3 <120284650+johnfitz3@users.noreply.github.com>
2023-02-04 18:22:08 -0500

    updating repo

--- script.js
+++ script.js
@@ -15,47 +15,185 @@ $(document).ready(function(){
 });
 });
 
+
+
 $(document).ready(function(){
-    $(".ans1").click(function(){
+    $("#q1-ans1").click(function(){
       $(".question1").hide();
     });
-    $(".ans1").click(function(){
+    $("#q1-ans1").click(function(){
+        $(".question2").show();
+
+});
+});
+$(document).ready(function(){
+    $("#q1-ans2").click(function(){
+      $(".question1").hide();
+    });
+    $("#q1-ans2").click(function(){
+        $(".question2").show();
+
+});
+});
+$(document).ready(function(){
+    $("#q1-ans3").click(function(){
+      $(".question1").hide();
+    });
+    $("#q1-ans3").click(function(){
+        $(".question2").show();
+
+});
+});
+$(document).ready(function(){
+    $("#q1-ans4").click(function(){
+      $(".question1").hide();
+    });
+    $("#q1-ans4").click(function(){
         $(".question2").show();
 
 });
 });
 
 
+
+
+
+
+
+
+
 $(document).ready(function(){
-    $("#ans2").click(function(){
+    $("#q2-ans1").click(function(){
       $(".question2").hide();
     });
-    $("#ans2").click(function(){
+    $("#q2-ans1").click(function(){
+        $(".question3").show();
+
+});
+});
+$(document).ready(function(){
+    $("#q2-ans2").click(function(){
+      $(".question2").hide();
+    });
+    $("#q2-ans2").click(function(){
+        $(".question3").show();
+
+});
+});
+$(document).ready(function(){
+    $("#q2-ans3").click(function(){
+      $(".question2").hide();
+    });
+    $("#q2-ans3").click(function(){
+        $(".question3").show();
+
+});
+});
+$(document).ready(function(){
+    $("#q2-ans4").click(function(){
+      $(".question2").hide();
+    });
+    $("#q2-ans4").click(function(){
         $(".question3").show();
 
 });
 });
 
+
+
+
+
+
+
+
 $(document).ready(function(){
-    $("#ans3").click(function(){
+    $("#q3-ans1").click(function(){
       $(".question3").hide();
     });
 
 
 
-    $("#ans3").click(function(){
+    $("#q3-ans1").click(function(){
+        $(".question4").show();
+
+});
+});
+$(document).ready(function(){
+    $("#q3-ans2").click(function(){
+      $(".question3").hide();
+    });
+
+
+
+    $("#q3-ans2").click(function(){
+        $(".question4").show();
+
+});
+});
+$(document).ready(function(){
+    $("#q3-ans3").click(function(){
+      $(".question3").hide();
+    });
+
+
+
+    $("#q3-ans3").click(function(){
+        $(".question4").show();
+
+});
+});
+$(document).ready(function(){
+    $("#q3-ans4").click(function(){
+      $(".question3").hide();
+    });
+
+
+
+    $("#q3-ans4").click(function(){
         $(".question4").show();
 
 });
 });
 
+
+
+
+
+
 $(document).ready(function(){
-    $("#ans4").click(function(){
+    $("#q4-ans1").click(function(){
       $(".question4").hide();
     });
-    $("#ans4").click(function(){
+    $("#q4-ans1").click(function(){
         $(".question5").show();
 
 });
 });
 
+$(document).ready(function(){
+    $("#q4-ans2").click(function(){
+      $(".question4").hide();
+    });
+    $("#q4-ans2").click(function(){
+        $(".question5").show();
+
+});
+});
+$(document).ready(function(){
+    $("#q4-ans3").click(function(){
+      $(".question4").hide();
+    });
+    $("#q4-ans3").click(function(){
+        $(".question5").show();
+
+});
+});
+$(document).ready(function(){
+    $("#q4-ans4").click(function(){
+      $(".question4").hide();
+    });
+    $("#q4-ans4").click(function(){
+        $(".question5").show();
+
+});
+});
