a3e320f898be5d57f2af85cf72c446acefe6ba71
johnfitz3 <120284650+johnfitz3@users.noreply.github.com>
2023-02-04 21:05:07 -0500

    updating repo

--- script.js
+++ script.js
@@ -4,6 +4,24 @@ $(".question3").hide();
 $(".question4").hide();
 $(".question5").hide();
 
+var timer2 = "01:00";
+var interval = setInterval(function() {
+
+
+  var timer = timer2.split(':');
+ 
+  var minutes = parseInt(timer[0], 10);
+  var seconds = parseInt(timer[1], 10);
+  --seconds;
+  minutes = (seconds < 0) ? --minutes : minutes;
+  if (minutes < 0) clearInterval(interval);
+  seconds = (seconds < 0) ? 59 : seconds;
+  seconds = (seconds < 10) ? '0' + seconds : seconds;
+
+  $('.countdown').html(minutes + ':' + seconds);
+  timer2 = minutes + ':' + seconds;
+}, 1000);
+
 
 $(document).ready(function(){
     $("button").click(function(){
